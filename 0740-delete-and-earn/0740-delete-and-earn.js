b873f2b381e78c9c6dc568268150b3c48fa0deca
Karlos Ye <58801593+karlosye@users.noreply.github.com>
2022-10-20 13:43:02 -0400

    Time: 112 ms (68.52%), Space: 44.8 MB (86.34%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.js
+++ 0740-delete-and-earn/0740-delete-and-earn.js
@@ -6,34 +6,21 @@
 // hint: hashmap + sorting the array and remove duplicate
 
 var deleteAndEarn = function(nums) {
+    
+    if (nums.length ==1) {return nums[0]}
+    
+    const n = Math.max(...nums)+1;
+    const counts = new Array(n).fill(0);
+    
+    for (const num of nums) counts[num]++;
 
-    let hm = {};
-    
-    for (let number of nums) {
-        
-        hm[number] = (hm[number]||0) + 1;
+    const dp = new Array(n).fill(0);
+    dp[1] = counts[1];
+
+    for (let i = 2; i < n; i++) {
+        const points = counts[i] * i;
+        dp[i] = Math.max(dp[i - 2] + points, dp[i - 1]);
     }
     
-    nums = nums.filter( (item,index) => nums.indexOf(item) === index );
-    nums = nums.sort(function(a,b) {return a-b});
-    
-    if (nums.length ==1) {return nums[0] * hm[nums[0]]}
-    
-    let dp = [];
-    dp[0] = nums[0] * hm[nums[0]];
-    dp[1] = nums[1]==nums[0]+1? Math.max(nums[1]*hm[nums[1]],dp[0]) : dp[0]+nums[1]*hm[nums[1]];
-    
-    for(let i=2; i<nums.length;i++) {
-        
-        if (nums[i] == nums[i-1] + 1) {
-            
-            dp[i] = Math.max(nums[i]*hm[nums[i]] + dp[i-2] , dp[i-1]);
-            
-        } else {
-            
-            dp[i] = dp[i-1] + nums[i]*hm[nums[i]];
-        }
-    }
-    
-    return dp[dp.length-1];
-};
+    return dp[n - 1];
+}
