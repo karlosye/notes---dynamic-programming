d9d78f65a8225c13648d1d9e2a0e6e88ebe07095
Karlos Ye <58801593+karlosye@users.noreply.github.com>
2022-10-23 12:07:02 -0400

    Time: 116 ms (71.14%), Space: 46.3 MB (62.87%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+
+// using a greedy approach this time
+
+var canJump = function(nums) {
+    
+    let goal = nums.length - 1;
+    
+    for (let i=goal-1; i>=0; i--) {
+        
+        if (i + nums[i] >= goal) {
+            
+            goal = i;
+        }
+    }
+
+    return goal == 0;
+};
