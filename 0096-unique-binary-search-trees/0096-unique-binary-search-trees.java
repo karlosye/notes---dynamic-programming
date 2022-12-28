ddbfcf2af19a057f705e3edf92e0e9e55b71db95
Karlos Ye <58801593+karlosye@users.noreply.github.com>
2022-12-27 20:39:30 -0500

    Time: 0 ms (100.00%), Space: 39.1 MB (82.03%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,14 +1,25 @@
 // BST: left val < root.val and right val > root.val
 // recursive approach: dfs (top-down approach)
 
-// use a for-loop to loop from i to n; imagine having each node as a root node -> left number of nodes = i-1 and right = n-i; then sum these up
+// try if you can add a cache
 
 class Solution {
     
+    int[] cache = new int[20];
+    
     public int numTrees(int n) {
+        
+        // check if this n exists in cache:
+        if (this.cache[n] != 0) {
+            
+            return this.cache[n];
+        }
         
         // base case:
         if (n == 0 || n == 1) {
+            
+            this.cache[n] = 1;
+            
             return 1;
         }
         
@@ -21,6 +32,8 @@ class Solution {
             sum += numTrees(i-1) * numTrees(n-i);            
         }
         
-        return sum;
+        this.cache[n] = sum;
+        
+        return this.cache[n];
     }
 }
