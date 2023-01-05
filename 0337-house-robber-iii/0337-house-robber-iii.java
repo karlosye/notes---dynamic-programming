4022e110226102c9f14bb83c557c639a4a061893
Karlos Ye <58801593+karlosye@users.noreply.github.com>
2023-01-04 21:01:35 -0500

    Time: 0 ms (100.00%), Space: 41.8 MB (89.09%) - LeetHub

---
+++ 0337-house-robber-iii/0337-house-robber-iii.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+// use DFS: recursive function to return int[2]: withRoot and withoutRoot;
+
+// withRoot = max(withoutRoot_left) + max(withoutRoot_right);
+// withoutRoot = max(left) + max(right)
+
+class Solution {
+    public int rob(TreeNode root) {
+        
+        int[] res = this.dfs(root);
+        
+        return res[0] > res[1] ? res[0] : res[1];
+    }
+    
+    // define the dfs method:
+    public int[] dfs(TreeNode root) {
+        
+        // base case: null root
+        if (root == null) {return new int[]{0,0};}
+        
+        // recursive case: find the with-without from left and right; then return the max
+        int[] left = dfs(root.left);
+        int[] right = dfs(root.right);
+        
+        int withRoot = left[1] + right[1] + root.val;
+        
+        int leftMax = left[0] > left[1] ? left[0]:left[1];
+        int rightMax = right[0] > right[1] ? right[0]:right[1];
+        
+        int withoutRoot = leftMax + rightMax;
+        
+        return new int[]{withRoot, withoutRoot};
+    }
+}
